116cb2efc132b130d37c50d857cd8fbea9a6bed8
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-02 23:39:31 +0530

    FrontEnd Converted To Vite

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './components/home/home'
+import Navbar from './components/navbar/navbar'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
