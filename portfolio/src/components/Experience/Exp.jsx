c857154e679c8a91e21af9450cfb1f96a3a8550a
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-06 23:59:11 +0530

    exp

--- portfolio/src/components/Experience/Exp.jsx
+++ portfolio/src/components/Experience/Exp.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 
 function Exp() {
   return (
-    <div className='text-center mt-1 bg-gray-600'>
-      Expericne
+    <div class="bg-white rounded-lg shadow-lg p-8">
+  <h2 class="text-2xl font-bold text-gray-800">
+    Company Name
+  </h2>
+  <div class="mt-4 text-lg text-gray-700">
+    Position Title
+  </div>
+  <div class="mt-4 flex items-center justify-between">
+    <div class="text-gray-700">
+      January 2020 - Present
     </div>
+    <div class="text-gray-700">
+      Location
+    </div>
+  </div>
+  <div class="mt-8 text-gray-700">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Phasellus malesuada, quam a vulputate dictum, diam urna 
+      fermentum arcu, a scelerisque quam eros eget velit. 
+    </p>
+    <ul class="mt-4">
+      <li>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </li>
+      <li>
+        Phasellus malesuada, quam a vulputate dictum, diam urna fermentum arcu, a scelerisque quam eros eget velit.
+      </li>
+      <li>
+        Donec sollicitudin velit vel erat tincidunt, a porttitor lectus porttitor.
+      </li>
+    </ul>
+  </div>
+</div>
+
   )
 }
 
