7cb1c4363d11aaa8b49541cd3b6ae2af25bba729
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-07 23:00:41 +0530

    Changed Home (Err)

--- portfolio/src/components/home/home.jsx
+++ portfolio/src/components/home/home.jsx
@@ -4,42 +4,26 @@ import bgimg from '../../assets/pexels-photo-6739698.jpeg'
 function Home() {
   return (
     <div>
-<main class="py-16 bg-gray-100">
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="lg:grid lg:grid-cols-3 lg:gap-8">
-      <div class="mb-16 lg:mb-0">
-        <h2 class="text-2xl font-bold text-gray-900">
-          Section 1
-        </h2>
-        <p class="mt-4 text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Phasellus malesuada, quam a vulputate dictum, diam urna 
-          fermentum arcu, a scelerisque quam eros eget velit. 
+
+      <div className="bg-gray-300 min-h-screen p-6 flex flex-col justify-center">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+          Introduction
+        </h1>
+        <p className="text-xl text-gray-800 mb-6 leading-relaxed">
+          Hello, my name is John Doe and I am a software developer. I have experience in building web applications using a variety of technologies, including the MERN stack.
         </p>
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">Skills</h2>
+        <ul className="list-inside list-disc text-lg text-gray-800 mb-6">
+          <li>React</li>
+          <li>Node.js</li>
+          <li>MongoDB</li>
+          <li>Express</li>
+          <li>JavaScript</li>
+        </ul>
+        <a href="#" className="btn bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">
+          Contact Me
+        </a>
       </div>
-      <div class="mb-16 lg:mb-0">
-        <h2 class="text-2xl font-bold text-gray-900">
-          Section 2
-        </h2>
-        <p class="mt-4 text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Phasellus malesuada, quam a vulputate dictum, diam urna 
-          fermentum arcu, a scelerisque quam eros eget velit. 
-        </p>
-      </div>
-      <div class="mb-16 lg:mb-0">
-        <h2 class="text-2xl font-bold text-gray-900">
-          Section 3
-        </h2>
-        <p class="mt-4 text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Phasellus malesuada, quam a vulputate dictum, diam urna 
-          fermentum arcu, a scelerisque quam eros eget velit. 
-        </p>
-      </div>
-    </div>
-  </div>
-</main>
 
 
 
--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../../App.css'
 
 function navbar() {
   return (
     <div>
             <nav class="flex items-center justify-between px-4 py-3 bg-white fixed w-full z-50 top-0">
   <div class="flex items-center">
-    <a href="#" class="text-gray-800 font-bold text-xl">My App</a>
+    <a href="#" class="text-gray-800 font-bold text-xl text-fade-in">My App</a>
   </div>
   <div class="lg:hidden">
     <button class="block px-3 py-2 rounded-md text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100" aria-label="Menu">
